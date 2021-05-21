3fd9346351147f336a29b23b85ccf39eb6a93f67
albinvll <albin.vllahiu@gmail.com>
2021-05-21 22:24:56 +0200

    Backend + Sp initial commit

---
+++ UberFoodsAPI/PublicClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UberFoodsAPI
+{
+    public static class PublicClass
+    {
+        public static string ConnectionString = "Server=DESKTOP-8F04IE5\\SQLEXPRESS;Database=UBF;Integrated Security=True;";
+    }
+}
