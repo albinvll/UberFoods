1482a632310363af6cf34c664d7a26cf93c0aa81
albinvll <albin.vllahiu@gmail.com>
2021-05-21 22:47:02 +0200

    ArticlesController

--- UberFoodsAPI/UberFoodsAPI/Controllers/ArticlesController.cs
+++ UberFoodsAPI/UberFoodsAPI/Controllers/ArticlesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using UberFoodsAPI.Data;
 using UberFoodsAPI.Models;
 
 namespace UberFoodsAPI.Controllers
@@ -9,8 +12,18 @@ namespace UberFoodsAPI.Controllers
     public class ArticlesController : ControllerBase
     {
         [HttpGet("getArticlesFromRestaurantId")]
-        public List<Article> GetArticlesFromRestaurantId([FromQuery] int restaurantId) {
-            return null;
+        public List<Article> GetArticlesFromMenuId([FromQuery] int menuId) {
+            DataTable articlesTable = ArticlesData.GetArticlesWithMenuId(menuId);
+            List<Article> articlesList = new List<Article>(articlesTable.Rows.Count);
+            foreach(DataRow dr in articlesTable.Rows)
+            {
+                Article temp = new Article();
+                temp.Id = Convert.ToInt64(dr["Id"].ToString());
+                temp.Description = dr["Pershkrimi"].ToString();
+                temp.Price = Convert.ToInt64(dr["Cmimi"].ToString());
+                articlesList.Add(temp);
+            }
+            return articlesList;
         }
     }
 }
