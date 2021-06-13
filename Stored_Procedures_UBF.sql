56c110f400c72f6cd589713e95cc6047acacf2d5
Gjenis Alkovaç <76169691+Gjenis87@users.noreply.github.com>
2021-06-13 17:27:53 +0200

    Update Stored_Procedures_UBF.sql

--- Stored_Procedures_UBF.sql
+++ Stored_Procedures_UBF.sql
@@ -46,3 +46,42 @@ CREATE PROCEDURE PorositesiInsert_sp(
 	
 	INSERT INTO Porositesi VALUES(@PerdoruesiIRi,@MenyraPagesesId)
 END
+ 
+ CREATE PROCEDURE DerguesiInsert_sp(
+	@AdresaPershkrimi varchar(255),
+	@AdresaX decimal(18,12),
+	@AdresaY decimal(18,12),
+	@AdresaZ decimal(18,12),
+	@Emri varchar(255),
+	@Mbiemri varchar(255),
+	@Email varchar(255),
+	@AdresaPershkrimi varchar(255),
+	@MenyraDergesesPershkrimi varchar(255)
+
+
+ )AS BEGIN
+	declare @AdresaEReID bigint
+
+	INSERT INTO Adresa VALUES(@AdresaX,@AdresaY,@AdresaZ,@AdresaPershkrimi)
+
+	select @AdresaEReID=MAX(Id) from Adresa
+	
+	declare @PerdoruesiIRi bigint
+	
+	INSERT INTO Perdoruesi VALUES(@Emri,@Mbiemri,@Email,@AdresaEReID)
+
+	select @PerdoruesiIRi=MAX(Id) from Perdoruesi
+
+	declare @MenyraDergesesEReID bigint
+	
+	insert into MenyraDergeses Values (@Pershkrimi)
+
+	select MenyraDergesesEReID =MAX(id) from MenyraDergeses
+
+	insert into Derguesi values(@Perdoruesi,@MenyraDergeses)
+
+	End
+
+
+	
+	
