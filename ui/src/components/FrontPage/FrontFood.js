74e7121cbd45e859386564ee9837bbab4cc1a93b
albinvll <albin.vllahiu@gmail.com>
2021-07-04 12:56:29 +0200

    Update FrontFood.js

--- ui/src/components/FrontPage/FrontFood.js
+++ ui/src/components/FrontPage/FrontFood.js
@@ -3,71 +3,70 @@ import "./FrontFood.css";
 import Burger from "../../../src/assets/burder.jpg";
 import { connect } from "react-redux";
 import { addToCart } from "../../redux/food-order/actions";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import client from '../../axios';
-
-
-
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import client from "../../axios";
 
 const FrontFood = ({ addToCart }) => {
+	const [foods, setFoods] = useState([]);
 
-  const [foods, setFoods] = useState([]);
+	const notify = () => toast.success("Added to cart");
 
-  const notify = () => toast.success("Added to cart");
+	const fetchFoods = async () => {
+		if (localStorage.getItem("accountTypeId") == 1) {
+			const response = await client.get("Articles/getTop3Articles");
 
-  const fetchFoods= async ()=>{
-    const response = await client.get("Articles/getTop3Articles");
+		setFoods(response.data);
+		}
+	};
 
-    setFoods(response.data);
-  }
+	useEffect(() => {
+		fetchFoods();
+	}, []);
 
-  useEffect(()=>{
-    fetchFoods();
-  },[])
+	return (
+		<div className="mainSection">
+			<div className="titleSection">
+				<h1>
+					<strong>Uberfood</strong> is here for you!
+				</h1>
+				<p>
+					<strong>
+						Not sure what to order? There are some suggestion
+					</strong>
+				</p>
+			</div>
 
-  return (
-    <div className="mainSection">
-      <div className="titleSection">
-        <h1>
-          <strong>Uberfood</strong> is here for you!
-        </h1>
-        <p>
-          <strong>Not sure what to order? There are some suggestion</strong>
-        </p>
-      </div>
-
-      <div className="contentSection">
-        {foods.map((item, index) => (
-          <div className="food-div" key={index}>
-            <div className="food-img">
-              <img src={Burger} />
-            </div>
-            <div className="food-desc">
-              <h2>{item.description}</h2>
-              <h4>${item.price}</h4>
-              <button
-                id="button-food"
-                onClick={() => {
-                  addToCart(item);
-                  notify();
-                }}
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+			<div className="contentSection">
+				{foods.map((item, index) => (
+					<div className="food-div" key={index}>
+						<div className="food-img">
+							<img src={Burger} />
+						</div>
+						<div className="food-desc">
+							<h2>{item.description}</h2>
+							<h4>${item.price}</h4>
+							<button
+								id="button-food"
+								onClick={() => {
+									addToCart(item);
+									notify();
+								}}
+							>
+								Add to cart
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    addToCart: (item) => dispatch(addToCart(item)),
-  };
+	return {
+		addToCart: (item) => dispatch(addToCart(item)),
+	};
 };
 
 export default connect(null, mapDispatchToProps)(FrontFood);
